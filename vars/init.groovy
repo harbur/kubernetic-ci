b8acc84b10b470df4b122f36ff2e3b077a0fbfd0
spiddy <dimitris.kapanidis@harbur.io>
2018-03-30 18:36:31 +0200

    use kc

--- vars/init.groovy
+++ vars/init.groovy
@@ -3,11 +3,16 @@ def call(body) {
     try{
       stage ('Build') {
         checkout scm
-        checkout scm
-        //git branch: 'master', credentialsId: '12345-1234-4696-af25-123455', url: 'ssh://git@bitbucket.org:company/repo.git'
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'KUBERNETIC_ACCESS_KEY', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-          sh "echo $USERNAME - $PASSWORD ::::"
+
+        kc_image = docker.image("docker.k8s.harbur.io/kc")
+        kc_image.inside ()  {
+          sh("echo hello")
+          sh("kc build")
         }
+        
+        //withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'KUBERNETIC_ACCESS_KEY', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+          //sh "echo $USERNAME - $PASSWORD ::::"
+        //}
 
         sh "docker build -t demo ."
         echo "hello"
