f96fa1320c66022692702d40fe961f05af0392ac
spiddy <dimitris.kapanidis@harbur.io>
2018-03-30 18:11:28 +0200

    wip checkout private repo

--- vars/init.groovy
+++ vars/init.groovy
@@ -3,6 +3,12 @@ def call(body) {
     try{
       stage ('Build') {
         checkout scm
+        checkout scm
+        //git branch: 'master', credentialsId: '12345-1234-4696-af25-123455', url: 'ssh://git@bitbucket.org:company/repo.git'
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'KUBERNETIC_ACCESS_KEY', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+          sh "echo $USERNAME - $PASSWORD ::::"
+        }
+
         sh "docker build -t demo ."
         echo "hello"
       }
