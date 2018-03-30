b02e3fd4213d57208b576487443dfaff0e199e9a
spiddy <dimitris.kapanidis@harbur.io>
2018-03-30 19:14:28 +0200

    update kc version

--- vars/init.groovy
+++ vars/init.groovy
@@ -1,7 +1,7 @@
 def call(body) {
   node ("jenkins-jenkins-slave"){
     try{
-      kc_image = docker.image("docker.k8s.harbur.io/kc:1bff56a")
+      kc_image = docker.image("docker.k8s.harbur.io/kc:aa45cbb")
       kc_image.inside  {
 
         stage ('Checkout') {
