8b25bbb26a547f272380dfa62af9f9ba6668d484
spiddy <dimitris.kapanidis@harbur.io>
2018-03-30 18:59:36 +0200

    Added Push

--- vars/init.groovy
+++ vars/init.groovy
@@ -1,24 +1,24 @@
 def call(body) {
   node ("jenkins-jenkins-slave"){
     try{
-      stage ('Build') {
-        checkout scm
+      kc_image = docker.image("docker.k8s.harbur.io/kc:1bff56a")
 
-        kc_image = docker.image("docker.k8s.harbur.io/kc:1bff56a")
+      stage ('Checkout') {
+        checkout scm
+      }
+
+      stage ('Build') {
         kc_image.inside  {
             sh "kc build"
         }
-        
-        //withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'KUBERNETIC_ACCESS_KEY', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-          //sh "echo $USERNAME - $PASSWORD ::::"
-        //}
+      }
 
-        // sh "docker build -t demo ."
-        echo "hello"
-      }
       stage ('Push') {
-        echo "done"
+        kc_image.inside  {
+            sh "kc push"
+        }
       }
+
     } catch (e){
       throw e
     }
