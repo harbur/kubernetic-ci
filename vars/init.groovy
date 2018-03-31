20e62703ef7ef3a9883bb0245c3479d9a19ae42e
spiddy <dimitris.kapanidis@harbur.io>
2018-03-31 09:35:01 +0200

    Added registry credentials

--- vars/init.groovy
+++ vars/init.groovy
@@ -1,20 +1,22 @@
 def call(body) {
   node ("jenkins-jenkins-slave"){
     try{
-      kc_image = docker.image("docker.k8s.harbur.io/kc:04d0fc5")
-      kc_image.inside  {
-
-        stage ('Checkout') {
-          checkout scm
-        }
-
-        stage ('Build') {
-          sh "printenv"
-          sh "kc build -t ${BRANCH_NAME}"
-        }
-
-        stage ('Push') {
-          sh "kc push -t ${BRANCH_NAME}"
+      docker.withServer('docker.k8s.harbur.io', 'REGISTRY') {
+        kc_image = docker.image("docker.k8s.harbur.io/kc:04d0fc5")
+        kc_image.inside  {
+  
+          stage ('Checkout') {
+            checkout scm
+          }
+  
+          stage ('Build') {
+            sh "printenv"
+            sh "kc build -t ${BRANCH_NAME}"
+          }
+  
+          stage ('Push') {
+            sh "kc push -t ${BRANCH_NAME}"
+          }
         }
       }
     } catch (e){
