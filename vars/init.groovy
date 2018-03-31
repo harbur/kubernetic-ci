dd557bdb5631f8685b131e7b22a853b9efb4a68a
spiddy <dimitris.kapanidis@harbur.io>
2018-03-31 10:10:45 +0200

    added pause step

--- vars/init.groovy
+++ vars/init.groovy
@@ -16,8 +16,18 @@ def call(body) {
             sh "whoami"
             sh "ls -la /usr/bin/docker"
             sh "ls -la /var/run/"
-            sh "/usr/bin/docker ps"
             sh "kc build -t ${BRANCH_NAME}"
+          }
+
+          stage('Input') {
+def userInput = input(
+ id: 'userInput', message: 'Let\'s promote?', parameters: [
+ [$class: 'TextParameterDefinition', defaultValue: 'uat', description: 'Environment', name: 'env'],
+ [$class: 'TextParameterDefinition', defaultValue: 'uat1', description: 'Target', name: 'target']
+])
+echo ("Env: "+userInput['env'])
+echo ("Target: "+userInput['target'])
+
           }
   
           stage ('Push') {
