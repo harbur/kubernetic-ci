3d9325c18a12ef74f4b5f4e1c99e1be0289a4dd0
spiddy <dimitris.kapanidis@harbur.io>
2018-03-31 09:50:58 +0200

    use proper home directory

--- vars/init.groovy
+++ vars/init.groovy
@@ -4,7 +4,7 @@ def call(body) {
       sh "docker login docker.k8s.harbur.io -u admin -p admin123"
       docker.withRegistry('https://docker.k8s.harbur.io', 'REGISTRY') {
         kc_image = docker.image("docker.k8s.harbur.io/kc:04d0fc5")
-        kc_image.inside('-v /home/jenkins/.docker/:/home/jenkins/.docker/')  {
+        kc_image.inside('-v /home/jenkins/.docker/:/root/.docker/')  {
   
           stage ('Checkout') {
             checkout scm
