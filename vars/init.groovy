e1df165b48d24381d5ec120b99530bb17ebf44cd
spiddy <dimitris.kapanidis@harbur.io>
2018-03-31 11:51:57 +0200

    build status should be in stage

--- vars/init.groovy
+++ vars/init.groovy
@@ -5,32 +5,35 @@ def call(body) {
       docker.withRegistry('https://docker.k8s.harbur.io', 'REGISTRY') {
         kc_image = docker.image("docker.k8s.harbur.io/kc:04d0fc5")
         kc_image.inside('-v /home/jenkins/.docker/:/root/.docker/ -v /usr/bin/docker:/usr/bin/docker')  {
+  
           stage ('Checkout') {
             checkout scm
           }
+  
+          stage ('Build') {
+            bitbucketStatusNotify(buildState: 'INPROGRESS', buildKey: 'build', buildName: 'Build')
 
-          bitbucketStatusNotify(buildState: 'INPROGRESS', buildKey: 'build', buildName: 'Build')
-          try {
-            stage ('Build') {
+            try {
               sh "kc build -t ${BRANCH_NAME}"
+              bitbucketStatusNotify(buildState: 'SUCCESSFUL', buildKey: 'build', buildName: 'Build')
+            } catch(Exception e) {
+              bitbucketStatusNotify(buildState: 'FAILED', buildKey: 'build', buildName: 'Build',
+                buildDescription: 'Something went wrong with build!'
+              )
             }
-            bitbucketStatusNotify(buildState: 'SUCCESSFUL', buildKey: 'build', buildName: 'Build')
-          } catch(Exception e) {
-            bitbucketStatusNotify(buildState: 'FAILED', buildKey: 'build', buildName: 'Build',
-              buildDescription: 'Something went wrong with build!'
-            )
           }
-
-          bitbucketStatusNotify(buildState: 'INPROGRESS', buildKey: 'push', buildName: 'Push')
-          try {
-            stage ('Push') {
+  
+          stage ('Push') {
+            bitbucketStatusNotify(buildState: 'INPROGRESS', buildKey: 'push', buildName: 'Push')
+            try {
               sh "kc push -t ${BRANCH_NAME}"
+              bitbucketStatusNotify( buildState: 'SUCCESSFUL', buildKey: 'push', buildName: 'Push')
+            } catch(Exception e) {
+              bitbucketStatusNotify(buildState: 'FAILED', buildKey: 'push', buildName: 'Push',
+                buildDescription: 'Something went wrong while pushing image(s) to the registry!'
+              )
             }
-            bitbucketStatusNotify( buildState: 'SUCCESSFUL', buildKey: 'push', buildName: 'Push')
-          } catch(Exception e) {
-            bitbucketStatusNotify(buildState: 'FAILED', buildKey: 'push', buildName: 'Push',
-              buildDescription: 'Something went wrong while pushing image(s) to the registry!'
-            )
+
           }
         }
       }
