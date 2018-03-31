b2c0a4062241aa90262fd0dce3993e33118839b7
spiddy <dimitris.kapanidis@harbur.io>
2018-03-31 09:48:57 +0200

    pass docker config with volume to child container

--- vars/init.groovy
+++ vars/init.groovy
@@ -4,7 +4,7 @@ def call(body) {
       sh "docker login docker.k8s.harbur.io -u admin -p admin123"
       docker.withRegistry('https://docker.k8s.harbur.io', 'REGISTRY') {
         kc_image = docker.image("docker.k8s.harbur.io/kc:04d0fc5")
-        kc_image.inside  {
+        kc_image.inside('-v /home/jenkins/.docker/:/home/user')  {
   
           stage ('Checkout') {
             checkout scm
@@ -12,6 +12,7 @@ def call(body) {
   
           stage ('Build') {
             sh "printenv"
+            sh "pwd"
             sh "kc build -t ${BRANCH_NAME}"
           }
   
