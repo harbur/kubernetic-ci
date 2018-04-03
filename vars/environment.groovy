be183eb8e5e12247c6894234fbeb54a6d03d4270
spiddy <dimitris.kapanidis@harbur.io>
2018-04-04 00:34:13 +0200

    Remove intermediate image to use kc and helm
    They are now included in the kubernetic-slave image

--- vars/environment.groovy
+++ vars/environment.groovy
@@ -5,11 +5,11 @@ def call(body) {
       docker.withRegistry('https://docker.k8s.harbur.io', 'REGISTRY') {
         kc_image = docker.image("docker.k8s.harbur.io/kc:55688e4")
         kc_image.inside('-v /home/jenkins/.docker/:/root/.docker/ -v /usr/bin/docker:/usr/bin/docker')  {
-  
+
           stage ('Checkout') {
             checkout scm
           }
-  
+
           stage ('Deploy') {
             bitbucketStatusNotify(buildState: 'INPROGRESS', buildKey: 'deploy', buildName: 'Deploy')
 
@@ -21,7 +21,7 @@ def call(body) {
               bitbucketStatusNotify(buildState: 'FAILED', buildKey: 'deploy', buildName: 'Deploy',
                 buildDescription: 'Something went wrong with deploy!'
               )
-            }  
+            }
           }
         }
       }
--- vars/init.groovy
+++ vars/init.groovy
@@ -2,39 +2,32 @@ def call(body) {
   node ("jenkins-jenkins-slave"){
     try{
       sh "docker login docker.k8s.harbur.io -u admin -p sQe3tokiLm"
-      docker.withRegistry('https://docker.k8s.harbur.io', 'REGISTRY') {
-        kc_image = docker.image("docker.k8s.harbur.io/kc:55688e4")
-        kc_image.inside('-v /home/jenkins/.docker/:/root/.docker/ -v /usr/bin/docker:/usr/bin/docker')  {
   
-          stage ('Checkout') {
-            checkout scm
-          }
+      stage ('Checkout') {
+        checkout scm
+      }
   
-          stage ('Build') {
-            bitbucketStatusNotify(buildState: 'INPROGRESS', buildKey: 'build', buildName: 'Build')
-
-            try {
-              sh "kc build -t ${BRANCH_NAME}"
-              bitbucketStatusNotify(buildState: 'SUCCESSFUL', buildKey: 'build', buildName: 'Build')
-            } catch(Exception e) {
-              bitbucketStatusNotify(buildState: 'FAILED', buildKey: 'build', buildName: 'Build',
-                buildDescription: 'Something went wrong with build!'
-              )
-            }
-          }
+      stage ('Build') {
+        bitbucketStatusNotify(buildState: 'INPROGRESS', buildKey: 'build', buildName: 'Build')
+        try {
+          sh "kc build -t ${BRANCH_NAME}"
+          bitbucketStatusNotify(buildState: 'SUCCESSFUL', buildKey: 'build', buildName: 'Build')
+        } catch(Exception e) {
+          bitbucketStatusNotify(buildState: 'FAILED', buildKey: 'build', buildName: 'Build',
+            buildDescription: 'Something went wrong with build!'
+          )
+        }
+      }
   
-          stage ('Push') {
-            bitbucketStatusNotify(buildState: 'INPROGRESS', buildKey: 'push', buildName: 'Push')
-            try {
-              sh "kc push -t ${BRANCH_NAME}"
-              bitbucketStatusNotify( buildState: 'SUCCESSFUL', buildKey: 'push', buildName: 'Push')
-            } catch(Exception e) {
-              bitbucketStatusNotify(buildState: 'FAILED', buildKey: 'push', buildName: 'Push',
-                buildDescription: 'Something went wrong while pushing image(s) to the registry!'
-              )
-            }
-
-          }
+      stage ('Push') {
+        bitbucketStatusNotify(buildState: 'INPROGRESS', buildKey: 'push', buildName: 'Push')
+        try {
+          sh "kc push -t ${BRANCH_NAME}"
+          bitbucketStatusNotify( buildState: 'SUCCESSFUL', buildKey: 'push', buildName: 'Push')
+        } catch(Exception e) {
+          bitbucketStatusNotify(buildState: 'FAILED', buildKey: 'push', buildName: 'Push',
+            buildDescription: 'Something went wrong while pushing image(s) to the registry!'
+          )
         }
       }
     } catch (e){
