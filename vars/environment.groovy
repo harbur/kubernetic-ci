0c178a135b704d57d9c30eae89c03394151fe3be
spiddy <dimitris.kapanidis@harbur.io>
2018-04-04 00:51:05 +0200

    Remove intermediate image to get kc and helm

--- vars/environment.groovy
+++ vars/environment.groovy
@@ -2,27 +2,21 @@ def call(body) {
   node ("jenkins-jenkins-slave"){
     try{
       sh "docker login docker.k8s.harbur.io -u admin -p sQe3tokiLm"
-      docker.withRegistry('https://docker.k8s.harbur.io', 'REGISTRY') {
-        kc_image = docker.image("docker.k8s.harbur.io/kc:55688e4")
-        kc_image.inside('-v /home/jenkins/.docker/:/root/.docker/ -v /usr/bin/docker:/usr/bin/docker')  {
+        stage ('Checkout') {
+          checkout scm
+        }
 
-          stage ('Checkout') {
-            checkout scm
-          }
+        stage ('Deploy') {
+          bitbucketStatusNotify(buildState: 'INPROGRESS', buildKey: 'deploy', buildName: 'Deploy')
 
-          stage ('Deploy') {
-            bitbucketStatusNotify(buildState: 'INPROGRESS', buildKey: 'deploy', buildName: 'Deploy')
+        try {
+          sh "helm upgrade -i sandbox env/sandbox/"
 
-            try {
-              sh "helm upgrade -i sandbox env/sandbox/"
-
-              bitbucketStatusNotify(buildState: 'SUCCESSFUL', buildKey: 'deploy', buildName: 'Deploy')
-            } catch(Exception e) {
-              bitbucketStatusNotify(buildState: 'FAILED', buildKey: 'deploy', buildName: 'Deploy',
-                buildDescription: 'Something went wrong with deploy!'
-              )
-            }
-          }
+          bitbucketStatusNotify(buildState: 'SUCCESSFUL', buildKey: 'deploy', buildName: 'Deploy')
+        } catch(Exception e) {
+          bitbucketStatusNotify(buildState: 'FAILED', buildKey: 'deploy', buildName: 'Deploy',
+            buildDescription: 'Something went wrong with deploy!'
+          )
         }
       }
     } catch (e){
