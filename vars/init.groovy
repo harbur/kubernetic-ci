1b3c9248e78fc572756e08109643adb0ccb95e13
spiddy <dimitris.kapanidis@harbur.io>
2018-04-13 14:28:00 +0300

    Init now using dynamic stages

--- vars/init.groovy
+++ vars/init.groovy
@@ -1,24 +1,20 @@
 def call(body) {
 
   def docker = new io.harbur.cmds.Docker()
-  def git = new io.harbur.cmds.Git()
-  def helm = new io.harbur.cmds.Helm()
   def bitBucket = new io.harbur.cmds.BitBucket()
-  def kc = new io.harbur.cmds.KuberneticCLI()
+
+  def stages = new io.harbur.stages.Stages()
+  def charts = new io.harbur.stages.Charts()
+  def captain = new io.harbur.stages.Captain()
 
   node ("jenkins-jenkins-slave"){
     try{
       bitBucket.inProgress()
       docker.login("docker.k8s.harbur.io")
-      git.checkout()
 
-      stage ('Build') {
-        kc.build()
-      }
-
-      stage ('Push') {
-        kc.push()
-      }
+      stages.checkout()
+      captain.job()
+      charts.job()
 
       bitBucket.successful()
     } catch (e){
