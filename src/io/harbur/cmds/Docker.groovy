0d6bf9946d13a43ebffec048422dfccfa4f4b097
spiddy <dimitris.kapanidis@harbur.io>
2018-05-05 13:44:21 +0200

    update

--- src/io/harbur/cmds/Docker.groovy
+++ src/io/harbur/cmds/Docker.groovy
@@ -26,11 +26,7 @@ def build() {
   """
 
   for (docker in properties.project().docker) {
-    FOO = sh (script: "echo docker build -t ${docker.image}:${docker.version} -f ${docker.path} ${docker.context}", returnStdout: true)
-    sh "echo ${FOO}"
-    command = evaluate(println("docker build -t ${docker.image}:${docker.version} -f ${docker.path} ${docker.context}"))
-    sh "echo: ${command}"
-    sh "${command}"
+    FOO = sh (script: "echo docker build", returnStdout: true)
   }
 }
 
