7f86d858a94d66660428617697d93e8afa5626b3
spiddy <dimitris.kapanidis@harbur.io>
2018-05-05 15:32:00 +0200

    Added GIT_COMMIT_SHORT support

--- src/io/harbur/cmds/Git.groovy
+++ src/io/harbur/cmds/Git.groovy
@@ -8,6 +8,7 @@ def checkout() {
 
     // Make SCM variables available in environment
     env.GIT_COMMIT=scmVars.GIT_COMMIT
+    env.GIT_COMMIT_SHORT=scmVars.GIT_COMMIT[0..6]
     env.GIT_BRANCH=scmVars.GIT_BRANCH
     env.GIT_PREVIOUS_COMMIT=scmVars.GIT_PREVIOUS_COMMIT
     env.GIT_PREVIOUS_SUCCESSFUL_COMMIT=scmVars.GIT_PREVIOUS_SUCCESSFUL_COMMIT
