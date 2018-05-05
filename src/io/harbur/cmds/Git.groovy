53373e960ce877f5c4ef00e2a140e15456bd3368
spiddy <dimitris.kapanidis@harbur.io>
2018-05-05 15:00:39 +0200

    Make SCM variables available in env

--- src/io/harbur/cmds/Git.groovy
+++ src/io/harbur/cmds/Git.groovy
@@ -5,8 +5,12 @@ def checkout() {
   stage ('Checkout') {
     def scmVars = checkout scm
     def commitHash = scmVars.GIT_COMMIT
-    env.GIT_COMMIT=commitHash
-    println (scmVars)
-    sh """env"""
+
+    // Make SCM variables available in environment
+    env.GIT_COMMIT=scmVars.GIT_COMMIT
+    env.GIT_BRANCH=scmVars.GIT_BRANCH
+    env.GIT_PREVIOUS_COMMIT=scmVars.GIT_PREVIOUS_COMMIT
+    env.GIT_PREVIOUS_SUCCESSFUL_COMMIT=scmVars.GIT_PREVIOUS_SUCCESSFUL_COMMIT
+    env.GIT_URL=scmVars.GIT_URL
   }
 }
