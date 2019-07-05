0ff6e3c1dd989bbedd3c3cf015561d5c7c5b52c5
spiddy <dimitris.kapanidis@harbur.io>
2019-07-05 21:19:51 +0200

    add protocol to registry

--- src/io/harbur/cmds/Docker.groovy
+++ src/io/harbur/cmds/Docker.groovy
@@ -8,7 +8,7 @@ def build() {
   for (config in properties.project().docker) {
 
     echo "Authenticating to Registry ${registry}"
-    docker.withRegistry("eu.gcr.io", 'gcr:woven-computing-234012') {
+    docker.withRegistry("https://eu.gcr.io", 'gcr:woven-computing-234012') {
       def customImage = docker.build("${config.image}", "-f ${config.path} ${config.context}")
       
       if (config.tags) {
