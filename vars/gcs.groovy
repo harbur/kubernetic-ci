9fee1ceb1167d821aa28831b8a8218cc86822238
spiddy <dimitris.kapanidis@harbur.io>
2019-08-02 12:25:31 +0300

    add push to beta bucket on gcs

--- vars/gcs.groovy
+++ vars/gcs.groovy
@@ -2,6 +2,19 @@ def call(Map params) {
   pipeline {
     agent any
     stages {
+      stage('Push to beta GCS') {
+        when {
+          branch 'beta'
+        }
+        steps {
+         step([$class: "ClassicUploadStep",
+		credentialsId: "woven-computing-234012",
+		bucket:"gs://beta.${params.bucket}",
+		pattern: params.pattern,
+		pathPrefix: params.pathPrefix,
+		showInline: true])
+        }
+      }
       stage('Push to GCS') {
         when {
           branch 'master'
