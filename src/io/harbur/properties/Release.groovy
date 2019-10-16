10d959c27fa1122140d1cc4bf871f228c8057826
spiddy <dimitris.kapanidis@harbur.io>
2019-10-16 21:16:47 +0200

    add release values

--- src/io/harbur/properties/Release.groovy
+++ src/io/harbur/properties/Release.groovy
@@ -4,10 +4,12 @@ class Release {
     def name
     def namespace
     def path
+    List<String> values
 
     Release(yaml) {
         this.name = yaml.name
         this.namespace = yaml.namespace
         this.path = yaml.path
+        this.values = yaml.values
     }
 }
