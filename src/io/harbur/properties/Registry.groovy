330ade13aee01593d2c15de6caca56ecba40ae5b
spiddy <dimitris.kapanidis@harbur.io>
2019-11-25 01:11:34 +0100

    add registry.name

--- src/io/harbur/properties/Registry.groovy
+++ src/io/harbur/properties/Registry.groovy
@@ -2,10 +2,12 @@ package io.harbur.properties
 
 class Registry {
     def url
+    def name
     def credentialsId
 
     Registry(yaml) {
         this.url = yaml.url
+        this.name = yaml.name
         this.credentialsId = yaml.credentialsId
     }
 }
